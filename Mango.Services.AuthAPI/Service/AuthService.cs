e0908e3712cdfd68490432fe5f534acc572d3e79
Luis Carvalho <luishdc19@gmail.com>
2025-05-27 17:23:23 -0300

    Added service and service interface for Auth

---
+++ Mango.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,32 @@
+﻿using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Service.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(AppDbContext appDbContext, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _appDbContext = appDbContext;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO requestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDTO> Register(RegistrationRequestDTO requestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mango.Services.AuthAPI/Service/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Services.AuthAPI.Models.Dto;
+
+namespace Mango.Services.AuthAPI.Service.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<UserDTO> Register(RegistrationRequestDTO requestDTO);
+        Task<LoginResponseDTO> Login(LoginRequestDTO requestDTO);
+    }
+}
