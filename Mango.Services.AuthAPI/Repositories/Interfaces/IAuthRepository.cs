44ecb9eb28a8224e7de6ebcafc9e21712c9a2515
Luis Carvalho <luishdc19@gmail.com>
2025-05-27 17:39:45 -0300

    Implemented Register endpoint and added repository layer

---
+++ Mango.Services.AuthAPI/Repositories/Interfaces/IAuthRepository.cs
@@ -0,0 +1,9 @@
+﻿using Mango.Services.AuthAPI.Models;
+
+namespace Mango.Services.AuthAPI.Repositories.Interfaces
+{
+    public interface IAuthRepository
+    {
+        Task<ApplicationUser> GetApplicationUserByEmailAsync(string email);
+    }
+}
---
+++ Mango.Services.AuthAPI/Services/AuthService.cs
@@ -0,0 +1,65 @@
+﻿using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Repositories.Interfaces;
+using Mango.Services.AuthAPI.Service.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IAuthRepository _authRepository;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(IAuthRepository authRepository, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _authRepository = authRepository;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO requestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserDTO> Register(RegistrationRequestDTO requestDTO)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = requestDTO.Email,
+                Email = requestDTO.Email,
+                NormalizedEmail = requestDTO.Email.ToUpper(),
+                Name = requestDTO.Name,
+                PhoneNumber = requestDTO.PhoneNumber,
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(user, requestDTO.Password);
+
+                if (result.Succeeded)
+                {
+                    var userToReturn = await _authRepository.GetApplicationUserByEmailAsync(requestDTO.Email);
+
+                    var userDto = new UserDTO
+                    {
+                        Email = requestDTO.Email,
+                        ID = userToReturn.Id,
+                        Name = requestDTO.Name,
+                        PhoneNumber = requestDTO.PhoneNumber,
+                    };
+
+                    return userDto;
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return new UserDTO();
+        }
+    }
+}
