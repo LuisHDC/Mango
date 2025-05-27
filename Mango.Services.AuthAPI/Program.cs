d010f6991bf5b27d2d37fc84b8043f4541572ab6
Luis Carvalho <luishdc19@gmail.com>
2025-05-27 17:19:27 -0300

    Configured ApiSettings for AuthAPI

--- Mango.Services.AuthAPI/Program.cs
+++ Mango.Services.AuthAPI/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSettings:JwtOptions"));
 builder.Services.AddControllers();
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
